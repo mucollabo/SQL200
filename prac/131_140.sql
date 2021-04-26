ebc36e41937eabb5868d1e0390c95ea8f455b50c
Charles An <mucollabo@gmail.com>
2021-04-27 00:58:48 +0900

    study until sql200_135

--- prac/131_140.sql
+++ prac/131_140.sql
@@ -10276,3 +10276,45 @@ select 범죄유형
                     where term = '가정불화')
         and term = '가정불화';
         
+--134
+
+desc crime_cause2;
+
+select term as 원인
+    from crime_cause2
+    where cnt = (select max(cnt)
+                    from crime_cause2
+                    where 범죄유형 = '방화')
+        and 범죄유형 = '방화';
+
+
+--135
+
+create table acc_loc_data
+(ACC_LOC_NO     NUMBER(10),
+ ACC_YEAR       NUMBER(10),
+ ACC_TYPE       VARCHAR2(20),
+ ACC_LOC_CODE   NUMBER(10),
+ CITY_NAME      VARCHAR2(50),
+ ACC_LOC_NAME   VARCHAR2(200),
+ ACC_CNT        NUMBER(10),
+ AL_CNT         NUMBER(10),
+ DEAD_CNT       NUMBER(10),
+ M_INJURY_CNT   NUMBER(10),
+ L_INJURY_CNT   NUMBER(10),
+ H_INJURY_CNT   NUMBER(10),
+ LAT            NUMBER(10),
+ LOT            NUMBER(10),
+ DATA_UPDATE_DATE DATE);
+ 
+DESC ACC_LOC_DATA;
+
+SELECT * FROM ACC_LOC_DATA;
+
+SELECT *
+    FROM (SELECT ACC_LOC_NAME AS 사고장소, ACC_CNT AS 사고건수,
+                DENSE_RANK() OVER(ORDER BY ACC_CNT DESC NULLS LAST) AS 순위
+            FROM ACC_LOC_DATA
+            WHERE ACC_YEAR=2017)
+    WHERE 순위 <= 5;
+    
